ca309b2475410a6ad5b3799796495a4a951432fb
Dmytro V. Dogadailo <entropyhacker@gmail.com>
2013-08-04 22:44:33 +0300

    Added index.html stub.

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+fs = require('fs'),
+app = express();
+
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on' + port);
 });
